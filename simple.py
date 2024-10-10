ba1747aa9f9e5b74ed7d81e9ab703d9e72ea9ddb
Sourav Agarwal <2023ucp1627@mnit.ac.in>
2024-10-10 11:45:12 +0530

    Adding First File

---
+++ simple.py
@@ -0,0 +1,16 @@
+x=[]
+n=int(input("enter number of ele in first vec:"))
+for i in range(0,n,1):
+	a=int(input("enter ele:"))
+	x.append(a)
+y=[]
+print("Now for second vec:")
+for i in range(0,n,1):
+        a=int(input("enter ele:"))
+        y.append(a)
+d=0
+for i in range(0,n,1):
+        d+=x[i]*y[i]
+print(d)
+
+
